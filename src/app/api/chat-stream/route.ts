4c94de113436a4e0bb1e336e54184c28fe00a390
Ali Munir <alimunirwaris1@gmail.com>
2024-08-23 21:43:22 +0500

    Refactor POST function in chat-stream/route.ts

--- src/app/api/chat-stream/route.ts
+++ src/app/api/chat-stream/route.ts
@@ -3,15 +3,12 @@ import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
 import { NextResponse } from "next/server";
 
 
-export const POST = async (req: NextResponse) => {
+export async function POST(req: NextResponse) {
     const {messages,sessionId} = await req.json();
 
     const lastMessage = messages[messages.length - 1].content;
 
     const response = await ragChat.chat(lastMessage,{sessionId,streaming:true});
-    console.log('====================================');
-    console.log(response);
-    console.log('====================================');
 
     return aiUseChatAdapter(response);
 };
